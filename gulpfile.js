14b938fa994c4e3563927043fdcf5463d4e9096a
travis <t3patterson@gmail.com>
2016-08-04 22:40:39 -0500

    fixing gulpfile to automate the creation of the dist/ folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 "use strict"
 var gulp = require('gulp'),
     shell = require('gulp-shell'),
+    cleanCSS = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
     browserSync = require('browser-sync');
 
-gulp.task('build', 
+gulp.task('build-dev', 
   shell.task(['jekyll build --watch'])
 )
 
@@ -17,4 +19,24 @@ gulp.task('serve', function(){
   gulp.watch('_site/**/*.*')//.on('change', function(){ browserSync.reload() })
 })
 
-gulp.task('dev', ['build', 'serve']);
+gulp.task('minify-css', function(){
+  return gulp.src('./_site/css/main.css')
+    .pipe(cleanCSS())
+    .pipe(rename('margo.min.css'))
+    .pipe(gulp.dest('dist/margo-lib'))
+
+})
+
+gulp.task('build-production', shell.task([
+  'rm -rf ./dist/margo-lib; ', 
+  'mkdir ./dist/margo-lib',
+  'jekyll build',
+  'echo "done building...."',
+  'cp ./_site/css/main.css ./dist/margo-lib/main.css',
+  'cp -rf ./_sass/lib/* ./dist/margo-lib/'
+]))
+
+
+gulp.task('dev', ['build-dev', 'serve']);
+
+gulp.task('production', ['minify-css', 'build-production']);

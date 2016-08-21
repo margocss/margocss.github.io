8fde1fb8bcf23f4f81bea4a149f7f8b9fc62f9f4
travis <t3patterson@gmail.com>
2016-08-20 23:28:41 -0500

    refactored 'build-dist' script + recipes section for site

--- gulpfile.js
+++ gulpfile.js
@@ -20,20 +20,17 @@ gulp.task('serve', function(){
   gulp.watch('_site/**/*.*')//.on('change', function(){ browserSync.reload() })
 })
 
-gulp.task('minify-dist-css', ['run-autoprefix'], function(){
-  return gulp.src('./_site/css/main.css')
-    .pipe(cleanCSS())
-    .pipe(rename('margo.min.css'))
-    .pipe(gulp.dest('dist'))
-})
 
 gulp.task('minify-production-css', ['run-autoprefix'], function(){
   return gulp.src('./_site/css/main.css')
     .pipe(cleanCSS())
     .pipe(rename('main.min.css'))
     .pipe(gulp.dest('./_site/css'))
-
 })
+
+gulp.task('build-production', shell.task([
+    'jekyll build'
+]))
 
 gulp.task('run-autoprefix', function(){
   return gulp.src('./_site/css/main.css')
@@ -43,18 +40,21 @@ gulp.task('run-autoprefix', function(){
 
 
 
-gulp.task('build-production', shell.task([
-    'jekyll build'
-]))
+gulp.task('minify-dist-css', ['run-autoprefix-dist'], function(){
+  return gulp.src('./dist/margo.css')
+    .pipe(cleanCSS())
+    .pipe(rename('margo.min.css'))
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('run-autoprefix-dist', function(){
+  return gulp.src('./dist/margo.css')
+    .pipe(autoprefixer({ browsers: ['> 5%'] }))
+    .pipe(gulp.dest('./dist'))
+})
 
 
-gulp.task('build-dist', shell.task([
-  'rm -rf ./dist/*', 
-  'jekyll build',
-  'echo "done building...."',
-  'cp ./_site/css/main.css ./dist/margo.css',
-  'cp -rf ./_sass/lib/* ./dist/',
-]))
+
 
 
 
@@ -63,4 +63,4 @@ gulp.task('dev', ['build-dev', 'serve']);
 
 gulp.task('production', ['build-production', 'minify-production-css']);
 
-gulp.task('dist', ['build-dist', 'minify-dist-css']); 
+gulp.task('dist', ['minify-dist-css']); 
